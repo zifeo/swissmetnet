c828e732cbcca7eb6c3d65f3b356bec5c52e304d
Teo Stocco <teo@zifeo.com>
2021-08-02 00:35:52 +0200

    fix format

--- swissmetnet/utils.py
+++ swissmetnet/utils.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import pymongo
@@ -6,7 +7,9 @@ import pymongo
 def upsert_mongo(db, collection, df):
     logging.info(f"saving {collection}: {df.shape}")
     try:
-        db[collection].insert_many(df.to_dict(orient="records"), ordered=False)
+        db[collection].insert_many(
+            json.loads(df.to_json(orient="records")), ordered=False
+        )
     except pymongo.errors.BulkWriteError as err:
         if any([e["code"] != 11000 for e in err.details["writeErrors"]]):
             raise err
